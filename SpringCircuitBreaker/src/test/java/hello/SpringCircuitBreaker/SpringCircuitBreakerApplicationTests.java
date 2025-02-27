ffccf419442afc38c41a99e62e38a30c0be5b787
Anuragha-P <anuraghap2002@gmail.com>
2025-02-27 11:42:03 +0530

    circuitBreaker

---
+++ SpringCircuitBreaker/src/test/java/hello/SpringCircuitBreaker/SpringCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.SpringCircuitBreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
